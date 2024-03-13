c3be7ff9cb074d6787fb73f3be294f56bb81e617
Maghish <mail4mithran@gmail.com>
2024-03-13 20:58:49 +0530

    Converted JS to TS + Build, Start, Dev commands for the back-end server after TS integration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,12 @@
 // Load modules
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
 
 // Access environment variables from local .env file
-dotenv.config();
 // PORT is 4000 but for the MONGO_URI you may need to create your own database for testing this application and store it's uri in a .env file.
 // If you want to access the real database we use, then you better ask the project's admins or moderators for uri.
-PORT = process.env.PORT;
-MONGO_URI = process.env.MONGO_URI;
 
 // Initialize express application
 const app = express();
@@ -19,11 +16,13 @@ app.use(express.json());
 // Initialize cors middleware for allowing cross origin requests sharing for all websites
 app.use(cors());
 
+// app.get("/api", async (req, res) => {res.status(200).json({ message: "Hello world" })})
+
 // Connect MongoDB database with MongoDB database URI
-mongoose.connect(MONGO_URI).then(() => {
+mongoose.connect(process.env.MONGO_URI! as string).then(() => {
   console.log("Successfully connected to MongoDB");
   // Listen requests on PORT
-  app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
+  app.listen(4000, () => {
+    console.log(`listening on port ${4000}`);
   });
 });
